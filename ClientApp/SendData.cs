05c71468237c91d9994144846f18689e2ad11947
sparrow_n4k@mail.ru <sparrow_n4k@mail.ru>
2022-02-09 11:46:08 +0300

    Разработка логики отправки и получения данных для клиента

---
+++ ClientApp/SendData.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    internal class SendData
+    {
+        // адрес и порт сервера, к которому будем подключаться
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        
+        // отправка данных серверу
+        public void SendDataForServer() 
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+                // подключаемся к удаленному хосту
+
+                Console.Write("Введите сообщение:");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                // получаем ответ
+                data = new byte[256]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("ответ сервера: " + builder.ToString());
+
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+
+        // получение данных по id
+        public string[] GetDataToServerForId(string id)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+                // подключаемся к удаленному хосту
+                
+                byte[] data = Encoding.Unicode.GetBytes(id);
+                socket.Send(data);
+
+                // получаем ответ
+                data = new byte[256]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                
+                string dataStr = builder.ToString();
+                if (dataStr=="notData")
+                {
+                    return null;
+                }
+                else
+                {
+                    string[] serverDataArr = dataStr.Split("*");
+                    return serverDataArr;
+                }
+                
+                
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default;
+            }
+        }
+
+        // получение всех данных данных
+        public string[] GetAllDataToServer()
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+                // подключаемся к удаленному хосту
+
+                byte[] data = Encoding.Unicode.GetBytes("allData");
+                socket.Send(data);
+
+                // получаем ответ
+                data = new byte[256]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+
+                string dataStr = builder.ToString();
+                if (dataStr == "notData")
+                {
+                    return null;
+                }
+                else
+                {
+                    string[] serverDataArr = dataStr.Split("//");
+                    return serverDataArr;
+                }
+
+
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default;
+            }
+        }
+    }
+}
